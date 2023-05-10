b9e5fb88a28d8a676d892c25911310766a218a10
shiyiyuedeyu <36290871+shiyiyuedeyu@users.noreply.github.com>
2023-05-10 11:46:51 +0800

    Update ringbuffer.h

--- RingBuffer/ringbuffer.h
+++ RingBuffer/ringbuffer.h
@@ -1,1 +1,55 @@
 #pragma once
+
+#include <atomic>
+#include <mutex>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+template<typename T>
+class CycleQueue{
+ public:
+  // init size must > 1
+  explicit CycleQueue(int size = 1000) noexcept :
+    c_size(size), c_front(0), c_rear(0) {
+    c_data = new T[c_size];  
+  }
+  
+  ~CycleQueue() {
+    delete[] c_data;
+  }
+  
+  bool isEmpty() {
+    return c_front == c_rear;
+  }
+  
+  bool isFull() {
+    return c_front == (c_rear + 1) % c_size;
+  }
+  
+  void push(T ele) {
+    if (isFull()) {
+      pop();
+    }
+    c_data[c_rear] = ele;
+    c_rear = (c_rear + 1) % c_size;
+  }
+  
+  T pop() {
+    if (isEmpty()) {
+      return T();
+    }
+    T tmp = c_data[c_front];
+    c_front = (c_front + 1) % c_size;
+    return tmp;
+  }
+ private:
+  int c_size;
+  std::atomic<int> c_front;
+  std::atomic<int> c_rear;
+  T* c_data;
+};
+
+
+
+
